4843f71248522d68124c02ab4836b7c073981676
Kamil Suleimanov <38867359+krakatoor@users.noreply.github.com>
2022-02-15 00:54:37 +0300

    initial

--- Lone Wanderer/ContentView.swift
+++ Lone Wanderer/ContentView.swift
@@ -6,16 +6,19 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 42.96663, longitude: 47.51263), span: MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03))
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Map(coordinateRegion: $mapRegion)
+            .ignoresSafeArea()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environment(\.locale, .init(identifier: "ru"))
     }
 }
