7d63fe09bcd9358940afd066453a55a4a396c2ad
Kamil Suleimanov <38867359+krakatoor@users.noreply.github.com>
2022-02-15 00:40:58 +0300

    Initial Commit

---
+++ Lone Wanderer/Lone_WandererApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lone_WandererApp.swift
+//  Lone Wanderer
+//
+//  Created by Камиль Сулейманов on 15.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Lone_WandererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
